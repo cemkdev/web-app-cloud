ba040d9c84905aea6812aaa5d70a27a3ed919e89
cemkdev <79233363+cemkdev@users.noreply.github.com>
2026-05-25 01:45:59 +0300

    Add pending Serilog configuration extension scaffold

---
+++ WebAppAPI/Presentation/WebAppAPI.API/Extensions/Observability/SerilogConfigurationExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Serilog;
+using Serilog.Events;
+using WebAppAPI.API.Options.Observability;
+
+namespace WebAppAPI.API.Extensions.Observability
+{
+    public static class SerilogConfigurationExtensions
+    {
+        public static IHostBuilder UseConfiguredSerilog(
+            this IHostBuilder hostBuilder,
+            IConfiguration configuration,
+            IWebHostEnvironment environment)
+        {
+            if (hostBuilder is null)
+                throw new ArgumentNullException(nameof(hostBuilder));
+
+            if (configuration is null)
+                throw new ArgumentNullException(nameof(configuration));
+
+            if (environment is null)
+                throw new ArgumentNullException(nameof(environment));
+
+            return hostBuilder.UseSerilog((context, services, loggerConfiguration) =>
+            {
+                var observabilityOptions = configuration
+                                                .GetSection(ObservabilityOptions.SectionName)
+                                                .Get<ObservabilityOptions>()
+                                                ?? new ObservabilityOptions();
+
+                ConfigureMinimumLevels(loggerConfiguration);
+
+                loggerConfiguration
+                    .Enrich.FromLogContext()
+                    .Enrich.WithProperty("ApplicationName", observabilityOptions.ApplicationName)
+                    .Enrich.WithProperty("Environment", environment.EnvironmentName)
+                    .MinimumLevel.Information()
+                    .WriteTo.Console();
+            });
+        }
+
+        private static void ConfigureMinimumLevels(LoggerConfiguration loggerConfiguration)
+        {
+            loggerConfiguration
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning);
+        }
+    }
+}
