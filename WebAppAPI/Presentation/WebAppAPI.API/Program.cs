aa2ae8ba60641557c3c55853acfdf5bf76b2af33
cemkdev <79233363+cemkdev@users.noreply.github.com>
2026-06-08 15:18:03 +0300

    Clean up local secret configuration
    - Remove unused User Secrets configuration
    - Remove obsolete local HTTPS documentation and cert ignore entries
    - Clean up Program.cs regions
    - Keep public repo configuration aligned with local demo usage

--- WebAppAPI/Presentation/WebAppAPI.API/Program.cs
+++ WebAppAPI/Presentation/WebAppAPI.API/Program.cs
@@ -7,34 +7,24 @@ using WebAppAPI.Persistence.Seeding;
 
 var builder = WebApplication.CreateBuilder(args);
 
-if (builder.Environment.IsDevelopment())
-{
-    builder.Configuration.AddUserSecrets<Program>();
-}
-
-#region Services
 builder.Services.AddApiConfigurationOptions(builder.Configuration);
 builder.Services.AddWebAppApiDependencies(builder.Configuration);
 builder.Services.AddApiServices(builder.Configuration);
 builder.Services.AddApiObservability(builder.Configuration);
 builder.Services.AddApiAuthentication(builder.Configuration);
-#endregion
 
 var app = builder.Build();
 
-#region Seed Sample Entity Data
+// Seed demo data for local development and sample usage.
 using (var scope = app.Services.CreateScope())
 {
     var sp = scope.ServiceProvider;
     var userManager = sp.GetRequiredService<UserManager<AppUser>>();
     await DemoSeed.CheckAndSeedAsync(sp, userManager);
 }
-#endregion
 
-#region Middlewares
 app.UseApiSwagger();
 app.UseApiPipeline<Program>(builder.Configuration);
 app.MapApiEndpoints();
-#endregion
 
 app.Run();
